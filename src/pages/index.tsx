c563e8fe18adf292b803b6f0c5c3eca4181f42a0
James Wallington <jameswallington11@gmail.com>
2024-05-06 13:04:02 +0200

    TODO: case studies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 
-import Image from "next/image";
 import Navbar from "../components/Navbar";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardTitle,
-  CardFooter,
-  CardHeader,
-} from "../components/ui/card";
 import TechStack from "../components/techStack";
 import Intro from "../components/Intro";
-import RootLayout from "../layout";
-import NavItems from "../components/navItems";
 import GeneralInfo from "../components/generalInfo";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
 import { useState } from "react";
-import Link from "next/link";
 import { useRouter } from "next/router";
+
 import Footer from "../components/footer";
 
 export default function Home() {
